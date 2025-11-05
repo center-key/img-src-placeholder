15ea5119b5b719632617b301dd99380b7dd7e8a1
pilafmon <pilaf@centerkey.com>
2025-11-05 04:54:41 -0800

    Clean up spec folder structure

--- spec/mocha.spec.js
+++ spec/mocha.spec.js
@@ -52,9 +52,9 @@ describe('Library module', () => {
 describe('Calling imgSrcPlaceholder.transform()', () => {
 
    it('creates the correct text files in the target folder', () => {
-      const options = { cd: 'spec/fixtures' };
-      imgSrcPlaceholder.transform('source', 'target/cd', options);
-      const actual = cliArgvUtil.readFolder('spec/fixtures/target/cd');
+      const options = { cd: 'spec' };
+      imgSrcPlaceholder.transform('fixtures', 'target/cd', options);
+      const actual = cliArgvUtil.readFolder('spec/target/cd');
       const expected = [
          'mock1.html',
          'subfolder',
@@ -87,8 +87,8 @@ describe('Executing the CLI', () => {
    const run = (posix) => cliArgvUtil.run(pkg, posix);
 
    it('creates the expected file with the correct <img> placeholder', () => {
-      run('img-src-placeholder spec/fixtures/source/subfolder spec/fixtures/target/cli');
-      const html =     fs.readFileSync('spec/fixtures/target/cli/mock2.html', 'utf-8');
+      run('img-src-placeholder spec/fixtures/subfolder spec/target/cli');
+      const html =     fs.readFileSync('spec/target/cli/mock2.html', 'utf-8');
       const imgTag =   /<img [^>]*>/gm;
       const actual =   { tag: html.match(imgTag)[0] };
       const expected = { tag: '<img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxIiBoZWlnaHQ9IjEiPjwvc3ZnPg==" alt=placeholder>' };
