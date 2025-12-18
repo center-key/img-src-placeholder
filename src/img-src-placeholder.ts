bfd5e02d1aa9fd36309d506f8327f7618824983f
pilafmon <pilaf@centerkey.com>
2025-12-17 23:57:58 -0800

    More compact log messages

--- src/img-src-placeholder.ts
+++ src/img-src-placeholder.ts
@@ -91,14 +91,13 @@ const imgSrcPlaceholder = {
          };
       const settings = { ...defaults, ...options };
       const name =      chalk.gray('img-src-placeholder');
-      const source =    chalk.blue.bold(results.source);
-      const target =    chalk.magenta(results.target);
-      const arrow =     { big: chalk.gray.bold(' ⟹  '), little: chalk.gray.bold('→') };
+      const indent =    chalk.gray('|');
+      const ancestor =  cliArgvUtil.calcAncestor(results.source, results.target);
       const infoColor = results.count ? chalk.white : chalk.red.bold;
       const info =      infoColor(`(files: ${results.count}, ${results.duration}ms)`);
-      log(name, source, arrow.big, target, info);
+      log(name, ancestor.message, info);
       const logFile = (file: Results['files'][0]) =>
-         log(name, chalk.white(file.origin), arrow.little, chalk.green(file.dest));
+         log(name, indent, cliArgvUtil.calcAncestor(file.origin, file.dest).message);
       if (!settings.summaryOnly)
          results.files.forEach(logFile);
       return results;
