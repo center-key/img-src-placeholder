08d3b2456864f8a4194def73ca8058b7ac72cfe4
pilafmon <pilaf@centerkey.com>
2026-01-04 02:47:58 -0800

    Add placeholder count to log output

--- src/img-src-placeholder.ts
+++ src/img-src-placeholder.ts
@@ -91,13 +91,12 @@ const imgSrcPlaceholder = {
          };
       const settings = { ...defaults, ...options };
       const name =      chalk.gray('img-src-placeholder');
-      const indent =    chalk.gray('|');
       const ancestor =  cliArgvUtil.calcAncestor(results.source, results.target);
       const infoColor = results.count ? chalk.white : chalk.red.bold;
       const info =      infoColor(`(files: ${results.count}, ${results.duration}ms)`);
       log(name, ancestor.message, info);
-      const logFile = (file: Results['files'][0]) =>
-         log(name, indent, cliArgvUtil.calcAncestor(file.origin, file.dest).message);
+      const logFile = (file: Results['files'][0], i: number) =>
+         log(name, chalk.magenta(i + 1), cliArgvUtil.calcAncestor(file.origin, file.dest).message);
       if (!settings.summaryOnly)
          results.files.forEach(logFile);
       return results;
